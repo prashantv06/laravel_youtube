43f678d13e66e6aaebbb96cb1b0ba4ebf6acbcd2
Prashant <prasant.irl@gmail.com>
2020-07-26 12:47:58 +0100

    data fetched usinh youtube api

--- laravel_youtube/app/Http/Controllers/ApiController.php
+++ laravel_youtube/app/Http/Controllers/ApiController.php
@@ -8,28 +8,19 @@ use GuzzleHttp\Client;
 class ApiController extends Controller
 {
     public function index()
-    {
-    	/*$client = new Client();
-    	$response = $client->request('GET', 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBeToWz-t7fHQbINk0MqpS4amOXGbYQXWU&q=wordpress tutorial');
-    	$statusCode = $response->getStatusCode();
-        //$body = $response->getBody()->getContents();
-        $response->getBody();
-
-        return $response;*/
-        
+    {   
 
         $client = new Client();
-        $res = $client->request('GET', 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBeToWz-t7fHQbINk0MqpS4amOXGbYQXWU&q=wordpress tutorial');
+        $res = $client->request('GET', 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBeToWz-t7fHQbINk0MqpS4amOXGbYQXWU&q=wordpress tutorial&maxResults=20');
         $res->getStatusCode();
         // "200"
         $res->getHeader('content-type')[0];
         // 'application/json; charset=utf8'
+        
         $res_body =  $res->getBody();
 
-        //return view('Youtube',);
-        //return View::make('Youtube')->with('response', $res);
-
-        return view('Youtube')->with('response', $res_body)->header('Content-Type', 'text/plain');
-
+        //return view('Youtube')->with('response', $res_body_plain);
+        return view('Youtube')->with('response', $res_body);
+        //return view('Youtube', $res_body_plain);
     }
 }
--- laravel_youtube/resources/views/Youtube.blade.php
+++ laravel_youtube/resources/views/Youtube.blade.php
@@ -40,48 +40,90 @@
         }
       }
     </style>
-    <!-- Custom styles for this template -->
     <link href="starter-template.css" rel="stylesheet">
+
+
+    
+
   </head>
   <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">
+          <img src="http://placehold.it/150x50?text=Logo" alt="">
+    </a>
+  <a class="navbar-brand">YouTube</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu" aria-labelledby="dropdown01">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+  
+    <form class="form-inline my-2 my-lg-0 right">
+      <input class="form-control mr-sm-2" type="text" id="search_field" placeholder="Search" aria-label="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" id="yt_search" type="button">Search</button>
     </form>
+ 
   </div>
 </nav>
 
-<main role="main" class="container">
-      <div>
-        <p>{{ $response }}</p> 
-      </div>
-</main><!-- /.container -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script></body>
+
+<main style="margin-top:100px;" role="main" class="container">
+  <div>  
+        <div id="result" >
+          <div class="row"></div>
+        </div>
+  </div> 
+</main>
+
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+      <script src="/docs/4.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script></body>
+
+
+      <script type="text/javascript">
+
+//document.addEventListener('DOMContentLoaded',function(){
+var key_id = "AIzaSyBeToWz-t7fHQbINk0MqpS4amOXGbYQXWU";
+var part = "snippet";
+
+var maxResults = 20;
+
+   $('#yt_search').click(function(){
+     var q = document.getElementById('search_field').value;
+     var ajaxurl = 'https://www.googleapis.com/youtube/v3/search?part='+part+'&key='+key_id+'&q='+q;
+     console.log(ajaxurl);
+
+      $.ajax({
+        type: "GET",
+        url: ajaxurl,
+        dataType:"jsonp",
+        success: function(response){
+        //console.log(response);
+
+          if(response.items){
+            $("#result > .row").empty();
+            $.each(response.items, function(i,items){
+
+              //console.log(items);
+
+              var video_id=items.id.videoId;
+              var video_title=items.snippet.title;
+              // IFRAME Embed for YouTube
+              var video_frame="<div class='col-md-6 col-xs-12'><iframe width='100%' height='385' src='http://www.youtube.com/embed/"+video_id+"' frameborder='0' type='text/html'></iframe></div>";
+              $("#result > .row").append(video_frame); // Result
+
+             });
+          }
+          else{
+            $("#result > .row").html("<div id='no'>No Video</div>");
+          }
+        }
+      });
+
+
+  //});
+});
+</script>
+
+</body>
 </html>
